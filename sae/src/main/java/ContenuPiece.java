1d3e6b7fb787b1a795bf5fb2705c5c62a53a9707
ETauzi <eliott.tauzi@etu.univ-orleans.fr>
2026-04-30 11:33:50 +0200

    Merge branch 'Eliott' ajout des fonction de base sur les classes

--- sae/src/main/java/ContenuPiece.java
+++ sae/src/main/java/ContenuPiece.java
@@ -5,6 +5,13 @@ public class ContenuPiece {
     private Piece piece;
     private Couleur couleur;
 
+    public ContenuPiece(int qt,boolean supp, Piece p, Couleur coul){
+        this.quantite=qt;
+        this.enSupplement = supp;
+        this.piece = p;
+        this.couleur = coul;
+    }
+
     public int getQt() {
         return quantite;
     }
