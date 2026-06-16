b4b05dc2b5c4420400877e97c6a5e08023fa58d2
Monlouis <guewen.monlouis@etu.univ-orleans.fr>
2026-06-16 16:20:46 +0200

    modif nom de vue pour l implementer dans lesoption admin

--- sae/src/main/java/UI/vue/ModifBoiteVue.java
+++ sae/src/main/java/UI/vue/ModifBoiteVue.java
@@ -27,7 +27,7 @@ import App.PieceService;
 import App.Theme;
 import App.ThemeService;
 
-public class ModifBoitePersoVue extends BorderPane {
+public class ModifBoiteVue extends BorderPane {
 
     private final BoiteService boiteService;
     private final ThemeService themeService;
@@ -52,7 +52,7 @@ public class ModifBoitePersoVue extends BorderPane {
     
     private VBox sectionStatistiques;
 
-    public ModifBoitePersoVue(Boite boite, BoiteService boiteService, ThemeService themeService, PieceService pieceService, Runnable actionRetour) {
+    public ModifBoiteVue(Boite boite, BoiteService boiteService, ThemeService themeService, PieceService pieceService, Runnable actionRetour) {
         this.boiteService = boiteService;
         this.themeService = themeService;
         this.pieceService = pieceService;
